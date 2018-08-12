1974378fa39008175fbf545c2ab3abb6e26dc1a9
Bensalah <amine.be@outlook.com>
2018-08-12 19:46:44 +0200

    initial project

---
+++ Sources/Overlay.h
@@ -0,0 +1,18 @@
+//
+//  Overlay.h
+//  Overlay
+//
+//  Created by Amine Bensalah on 12/08/2018.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Overlay.
+FOUNDATION_EXPORT double Overlay_VersionNumber;
+
+//! Project version string for Overlay.
+FOUNDATION_EXPORT const unsigned char Overlay_VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Overlay/PublicHeader.h>
+
+
